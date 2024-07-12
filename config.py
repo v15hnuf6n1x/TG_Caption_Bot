4a3a6c6f88054061ea05b92abe81cf193c19632d
VISHNU <Vishnuvishva2434@gmail.com>
2024-07-12 12:12:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+      API_ID = int(os.environ.get("API_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
-      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
-      ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
+      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "V15HNUF6N1X")
+      ADMIN_ID = int(os.environ.get("ADMIN", 123476535 )) 
       DB_URL = os.environ.get("DATABASE_URL", "")
